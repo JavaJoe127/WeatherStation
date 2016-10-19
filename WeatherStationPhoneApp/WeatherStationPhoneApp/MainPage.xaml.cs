0185335870ffef45911cf23abc2d163b87b2392f
Joe <javajoe127@gmail.com>
2016-10-18 22:29:20 -0600

    Initial Commit
    Commit of WeatherStation, WeatherStationPhoneApp, and WeatherStationWeb

---
+++ WeatherStationPhoneApp/WeatherStationPhoneApp/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="Hach">
+//   2016
+// </copyright>
+// <summary>
+//   The Weather Station Phone App main page.
+// The WebView Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WeatherStationPhoneApp
+{
+    using System;
+    using System.Diagnostics;
+    using Windows.Phone.UI.Input;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Navigation;
+
+    public sealed partial class MainPage : Page
+    {
+        // TODO: Replace with your URL here.
+        private static readonly Uri HomeUri = new Uri("ms-appx-web:///Html/index.html", UriKind.Absolute);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            WebViewControl.Navigate(HomeUri);
+
+            HardwareButtons.BackPressed += this.MainPage_BackPressed;
+        }
+
+        /// <summary>
+        /// Invoked when this page is being navigated away.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page is navigating.</param>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            HardwareButtons.BackPressed -= this.MainPage_BackPressed;
+        }
+
+        /// <summary>
+        /// Overrides the back button press to navigate in the WebView's back stack instead of the application's.
+        /// </summary>
+        private void MainPage_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (WebViewControl.CanGoBack)
+            {
+                WebViewControl.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        private void Browser_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            if (!args.IsSuccess)
+            {
+                Debug.WriteLine("Navigation to this page failed, check your internet connection.");
+            }
+        }
+
+        /// <summary>
+        /// Navigates forward in the WebView's history.
+        /// </summary>
+        private void ForwardAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WebViewControl.CanGoForward)
+            {
+                WebViewControl.GoForward();
+            }
+        }
+
+        /// <summary>
+        /// Navigates to the initial home page.
+        /// </summary>
+        private void HomeAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            WebViewControl.Navigate(HomeUri);
+        }
+
+        /// <summary>
+        /// Web view control when load completed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WebViewControl_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WeatherStationWeb/WeatherStationReadDevice/WeatherStationReadDevice.cs
@@ -0,0 +1,111 @@
+﻿/// <summary>
+/// This class will read the serial port it is told and return a dictionary of tag, value pairs
+/// 
+/// </summary>
+
+namespace WeatherStationReadDevice
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO.Ports;
+
+    public class WeatherStationReadDevice
+    {
+        // this class will open port to weather station, accept current readings, and make values available
+        private SerialPort serialPort;
+        private string serialPortName;
+
+        /// <summary>
+        /// The constructor for this class
+        /// </summary>
+        /// <param name="portName">
+        /// The port name will be like "COM1"
+        /// </param>
+        public WeatherStationReadDevice(string portName)
+        {
+            if (string.IsNullOrEmpty(portName))
+            {
+                throw new ArgumentNullException("portName");
+            }
+            serialPortName = portName;
+        }
+
+        private void OpenSerialPort()
+        {
+            try
+            {
+                serialPort.BaudRate = 9600;
+                serialPort.DataBits = 8;
+                serialPort.Handshake = Handshake.RequestToSend;
+                serialPort.Parity = Parity.None;
+                serialPort.ReadTimeout = 500;
+                serialPort.StopBits = StopBits.One;
+                serialPort.WriteTimeout = 50;
+
+                serialPort.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error trying to open serial port", e);
+            }
+        }
+
+        private void ClosePort()
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        /// <summary>
+        /// The read serial port method
+        /// </summary>
+        /// <returns>
+        /// Returns a dictionary of values, key = tag name + units
+        /// </returns>
+        public IDictionary<string, string> ReadSerialPort()
+        {
+            var dictionaryTagAndValueUnit = new Dictionary<string, string>();
+
+            using (serialPort = new SerialPort(serialPortName))
+            {
+                OpenSerialPort();
+                var buffer = string.Empty;
+                do
+                {
+                    buffer = serialPort.ReadLine();
+                    if (buffer.Contains("Running"))
+                    {
+                        continue;
+                    }
+
+                    if (buffer.Contains("Error"))
+                    {
+                        throw new Exception(buffer);
+                    }
+
+                    var bufferArray = buffer.Split(',');
+                    if (!string.IsNullOrEmpty(bufferArray[0]))
+                    {
+                            
+                        var tagAndUnits = string.IsNullOrEmpty(bufferArray[2]) 
+                            ? string.Concat(bufferArray[0], "-", bufferArray[2])
+                            : bufferArray[0];
+
+                        var valueReturned = string.IsNullOrEmpty(bufferArray[1])
+                            ? string.Empty
+                            : bufferArray[1];
+
+                        dictionaryTagAndValueUnit.Add(tagAndUnits, valueReturned);
+                    }
+                } while (buffer[0] != '*');
+
+                serialPort.Close();
+            }
+
+            return dictionaryTagAndValueUnit;
+
+        }
+    }
+}
